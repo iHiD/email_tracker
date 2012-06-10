5a222cfe4b55bcdf174d9face23eaef4586b3ee3
Jeremy Walker <jeremy@meducation.net>
2012-06-10 22:30:46 +0100

    Added links.

--- app/models/email_tracker/link.rb
+++ app/models/email_tracker/link.rb
@@ -1,5 +1,10 @@
 module EmailTracker
   class Link < ActiveRecord::Base
-    # attr_accessible :title, :body
+  
+    has_many :instances, class_name: "EmailTracker::LinkInstance"
+  
+    def self.for_url(url)
+      self.find_or_create_by_url(url)
+    end
   end
 end
